d183cf84efcda929af0b931ec2e271413f05f6c2
Alina Sagaidak <74879251+LinaMalinaMeow@users.noreply.github.com>
2025-01-17 01:18:35 +0300

    chore: updates

--- modules/bot.py
+++ modules/bot.py
@@ -1,9 +1,6 @@
 from telebot import TeleBot
 from modules.env import env;
 from modules.llm import generate_response
-from modules.messagesHistory import MessagesHistory;
-
-MessagesHistory = MessagesHistory()
 
 def init_bot():
     bot = TeleBot(env.get('TG_API_KEY'))
@@ -14,10 +11,7 @@ def init_bot():
 
     @bot.message_handler()
     def echo_message(message):
-        user_id = message.from_user.id
-        history = MessagesHistory.get_user_history(user_id)
-        response = generate_response(prompt=message.text, history=history)
-        MessagesHistory.add_message(user_id=user_id, user_message=message.text, bot_message=response)
+        response = generate_response(prompt=message.text)
         bot.reply_to(message, response)
 
     bot.infinity_polling()
--- modules/llm.py
+++ modules/llm.py
@@ -1,22 +1,35 @@
 import ollama
 from modules.vectorstore import search_knowledge_base
 
-def generate_response(prompt, history):
+def generate_response(prompt):
     rag = search_knowledge_base(prompt)
-    print(rag)
-    rag_docs = rag.get('documents')[0]
+    distances = rag['distances'][0]
+    condition_distance_indexes = []
 
+    for i in range(0, len(distances)):
+        if (distances[i] < 185):
+            condition_distance_indexes.append(i);
+    
+    rag_docs = rag.get('documents')[0]
+    docs_for_context = []
+
+    for i in range(len(rag_docs)):
+        if (i in condition_distance_indexes):
+            docs_for_context.append(rag_docs[i])
+    
     context = ''
-    for doc in rag_docs:
+    for doc in docs_for_context:
         context = context + doc + '\n'
 
     prompt = """
         Ты консультант в клинике, тебе задают вопросы, отвечай на эти вопросы опираясь на информацию,
-        которая находится ниже в теге "<context>", если ответа на вопрос нет в информации, тогда извиняйся и предлагай клиенту
+        которая находится ниже в теге "<context>", если ответа на вопрос нет в информации, но он относится к клинике или медицине,
+        тогда извиняйся и предлагай клиенту
         обратиться на горячую линию по телефону +7-999-999-99-99.
-        Если ты понимаешь, что вопрос не относится к клинике, медицине, отвечай, что ты консультируешь только по вопросам клиники.
+        Если ты понимаешь, что вопрос не относится к клинике, медицине, отвечай, что ты консультируешь только по вопросам клиники, 
+        не нужно предлагать звонить на горячую линию клиники.
         Отвечай на вопросы кратко и четко, без лишней информации.
-        Общайся максимально вежливо и уважительно, не показывай и не рассказывай, что ты нейронка и у тебя есть какой-то контекст.
+        Общайся максимально вежливо и уважительно, не показывай и не рассказывай, что ты нейросеть и у тебя есть какой-то контекст.
         Вопрос на который тебе нужно ответить находится в теге <question>.
         <context>
         {}
